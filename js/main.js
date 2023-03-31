cfd929b2f1499240bebe65e72b6b5f3be304bffd
shkrobadasha <shkrobadaria@mail.ru>
2023-04-01 01:00:53 +0300

    7.16

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@ import{generatePhotos} from './data.js';
 import {renderPhoto} from './pictureDrawer.js';
 
 const post = generatePhotos(25);
+
 renderPhoto(post);
 //eslint-disable-next-line
+//Задание 7 часть 2
