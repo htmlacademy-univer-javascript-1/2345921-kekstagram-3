da4a4e2770c55d7a272bf523e5eb2f46f49bdbe5
shkrobadasha <shkrobadaria@mail.ru>
2023-04-29 14:29:17 +0300

    9.9

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import{generatePhotos} from './data.js';
-import {renderPhoto} from './picture-drawer.js';
+import {renderPhoto} from './photo-render.js';
 import './form.js';
 import './validation-for-comment.js';
+import './scale-controller.js';
+import './filter-controller.js';
 
 const post = generatePhotos(25);
 renderPhoto(post);
